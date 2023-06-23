acb1ce68207cb6974646b9ae486507423e2ad992
Shivam <bishtshivam101@gmail.com>
2023-06-23 18:27:53 +0530

    updated db address

--- server/src/services/mysql.js
+++ server/src/services/mysql.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 
 const db = mysql.createPool({
-    host : 'localhost',
-    user:'root',
-    password:'',
-    database: 'asia_pacific_logistics',
+    host : '217.21.95.104',
+    user:'u391245239_asia_root',
+    password:'Classvoid29',
+    database: 'u391245239_asia_pacific',
     connectionLimit: 200 
 })
 
